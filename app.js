b39b26ea4108c28b833212ef50ce23a9fe282335
mohit <mohitpardeshi729@gmail.com>
2023-02-10 15:04:54 +0530

    added all methods to article route

--- app.js
+++ app.js
@@ -1,32 +1,81 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const ejs = require('ejs')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-
-main().catch(err => console.log(err));
-async function main() {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
-
-}
+const connectToDb = async () => {
+    mongoose.set("strictQuery", false);
+    mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+connectToDb().catch(console.error);
 
 const articleSchema = new mongoose.Schema({
-    title:String,
-    content:String
-})
-const Article = new mongoose.model("Article",articleSchema);
+    title: String,
+    content: String,
+});
+const Article = mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({}, (err, foundArticles) => {
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+            res.send(foundArticles);
+        });
+    })
+    .post((req, res) => {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("sucess")
+            } else {
+                res.send(err.message)
+            }
+        })
+
+    })
+    .delete((req, res) => {
+        Article.deleteMany({}, (err) => {
+            if (!err) {
+                res.send("Deleted")
+            } else {
+                res.send(err.message)
+            }
+        })
+    })
 
 
+app.route("/articles/:title")
+     .get((req,res)=>{
+        Article.findOne({title:req.params.title},(err,foundList)=>{
+            if(!err){res.send(foundList)}
+            else{res.send(err.message)}
+        })
+    })
+    .put((req,res)=>{
+        Article.replaceOne(
+            {title:req.params.title},
+            {title:req.body.title,content:req.body.content},
+            (err)=>{res.send("added")}
+        )
+    })
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
